34ecf864f455abaaf7fd994ff4d8b29c49a33e84
Ahmet Melih Çalış <sengmelihcalis@gmail.com>
2025-03-10 21:35:57 +0300

    renk özelligi ekledim

--- rating.py
+++ rating.py
@@ -1,9 +1,11 @@
-def starrating(score, max_score=10, max_star=5):
-
+def starrating(score, max_score=10, max_star=5, color=False):
     star_count = round(score / max_score * max_star)
-
-    return "★" * star_count + "☆" * (max_star - star_count)
-
+    if color:
+        # Renkli yıldızlar
+        return "\033[1;33m★" * star_count + "\033[0m" + "☆" * (max_star - star_count)
+    else:
+        # Normal siyah yıldızlar
+        return "★" * star_count + "☆" * (max_star - star_count)
 
 def main():
     try:
@@ -13,7 +15,7 @@ def main():
         if score < 0 or score > 10:
             print("Lütfen 0 ile 10 arasında bir değer girebilir misin.")
         else:
-            star_rating = starrating(score)
+            star_rating = starrating(score, color=True)
             print("Yıldız puanlaması:", star_rating)
     except ValueError:
         print("Lütfen geçerli bir sayı girebilir misin?")
